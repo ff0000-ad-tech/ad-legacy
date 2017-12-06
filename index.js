254707a428a337fc8b0d43097009eab7962ce2ba
Derek Miranda <derek.miranda@ff0000.com>
2017-12-05 17:44:34 -0800

    Fixed errors found by Rollup loader

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import DateUtils from './lib/DateUtils'
 import Align_old from './lib/Align_old'
 import DateStates from './lib/DateStates'
-import DateUtils from './lib/DateUtils'
 import DcsExpandable from './lib/DcsExpandable'
 import Expandable_legacy from './lib/Expandable_legacy'
 import Markup_legacy from './lib/Markup_legacy'
